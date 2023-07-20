13e011ead67150853111f360fe5dd59fcdfba495
Kevin Smith <kevin.smith2.ext@bayer.com>
2023-07-20 05:43:38 -0400

    CSS style approaches

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "@mui/material/Button";
+import { IonButton } from "@ionic/react";
+import { styled } from "@mui/material/styles"; // I believe this is @emotion/styled included with mui
+import { theme } from "./index";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>Button Raw</button>
+      <hr />
+      Above this line are HTML elements <br />
+      Below this line are custom components
+      <hr />
+      <ButtonComponent text="Custom Component" />
+      <StyledButton>Theme Styled</StyledButton>
+      <hr />
+      Above this line are custom components <br />
+      Below this line are MUI components
+      <hr />
+      <Button>MUI Base</Button>
+      <Button variant="contained" color="primary">
+        MUI Primary
+      </Button>
+      <Button variant="contained" color="warning">
+        MUI Overridden
+      </Button>
+      <SimpleStyledButton>Simple Styled</SimpleStyledButton>
+      <ThemedButton variant="contained" color="warning">
+        Themed Button
+      </ThemedButton>
+      <hr />
+      Above this line are MUI components <br />
+      Below this line are Ionic components WITHOUT Ionic theming
+      <hr />
+      <IonButton>Ion Base</IonButton>
+      <IonButton color="primary">Ion Primary</IonButton>
+      <br />
+      <StyledIonButton>Styled Ion Button</StyledIonButton>
     </div>
   );
 }
+
+// HTML element as component with no style (ignores theme)
+function ButtonComponent(props) {
+  return <button>{props.text}</button>;
+}
+
+// MUI component with style override
+const SimpleStyledButton = styled(Button)({
+  color: "red",
+  border: "1px solid black",
+  height: "30px",
+});
+
+// HTML element as component with style
+const StyledButton = styled("button")(({ theme }) => ({
+  color: theme.palette.primary.main,
+  ":hover": { color: theme.palette.warning.main },
+  height: "30px",
+  width: "130px",
+}));
+
+// HTML element as component with styles defined in the theme
+const ThemedButton = styled("button", {
+  name: "ThemedButton",
+  slot: "Root",
+})();
+
+const StyledIonButton = styled(IonButton)`
+  &::part(native) {
+    background-color: ${theme.palette.secondary.light};
+  }
+`;
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { pink } from "@mui/material/colors";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+export const theme = createTheme({
+  palette: {
+    warning: {
+      main: pink[300],
+    },
+  },
+  components: {
+    ThemedButton: {
+      variants: [
+        {
+          props: { variant: "contained", color: "warning" },
+          style: {
+            border: "2px dashed darkred",
+          },
+        },
+      ],
+    },
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
